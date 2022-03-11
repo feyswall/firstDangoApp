fa0e14f06a992afac2b9afb165ccebf9f6be0342
mukhusin siraji <mukhusinsiraji@gmail.com>
2022-03-11 14:54:02 +0300

    one to many relation

--- store/models.py
+++ store/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Models):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
-
-class Collection(models.Model):
-    title = models.CharField(max_length=255)
-    products = models.ForeignKey()
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
 class Customer(models.Model):
@@ -39,12 +39,30 @@ class Order(models.Model):
         ('C', 'complete'),
         ('F', 'fail')
     ]
-    placed_at = models.DateTimeField(auto_now=True)
+    placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=255, choices=PAYMENT_CHOICES, default='p')
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+
+#
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    Cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
