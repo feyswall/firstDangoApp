7058180f2db3c156a5bfd74679b9a39575723811
mukhusin siraji <mukhusinsiraji@gmail.com>
2022-03-11 14:36:10 +0300

    up to create models

---
+++ store/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Models):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    products = models.ForeignKey()
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_SILVER = 'S'
+
+    MEMBERSHIP_CHOICE = [
+        (MEMBERSHIP_BRONZE, 'BRONZE'),
+        (MEMBERSHIP_GOLD, 'GOLD'),
+        (MEMBERSHIP_SILVER, 'SILVER'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, choices=MEMBERSHIP_CHOICE, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_CHOICES = [
+        ('P', 'pending'),
+        ('C', 'complete'),
+        ('F', 'fail')
+    ]
+    placed_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(max_length=255, choices=PAYMENT_CHOICES, default='p')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE )
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
