1e1ecd0c649ac989ac49b7d8d371c2151060954c
mukhusin siraji <mukhusinsiraji@gmail.com>
2022-03-11 14:59:39 +0300

    many to many

--- store/models.py
+++ store/models.py
@@ -3,17 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
 
-class Product(models.Models):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    # promotions = models.ManyToManyField(Promotion, related_name='product')
+    promotions = models.ManyToManyField(Promotion, related_name='product')
 
 
 class Customer(models.Model):
