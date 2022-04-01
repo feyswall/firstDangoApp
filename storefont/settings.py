2b42944d1e87b4e0eb16b91281481f8c55b03a57
feyswall <vicentmkota91@gmail.com>
2022-04-01 13:59:41 +0300

    store font just now

--- storefont/settings.py
+++ storefont/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'storefont.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myTeacher',
+        'HOST': 'localhost',
+        'PASSWORD': 'pcb',
+        'PORT': '3306',
+        'USER': 'root',
     }
 }
 
